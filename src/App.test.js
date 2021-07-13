6a4db355f37aa82fd8152ae754c3aa9917e4750f
yoonseules <yoonseules@naver.com>
2021-07-13 10:38:47 +0900

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,13 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
